ad51472063e8e504e36330d74df21f20dd7d2821
MavSoccer1417 <74435341+MavSoccer1417@users.noreply.github.com>
2026-06-07 12:21:42 -0700

    integrate automated astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config'; // Added '/config' explicitly
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://elevatewithandy.com',
   base: '/',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
 });
